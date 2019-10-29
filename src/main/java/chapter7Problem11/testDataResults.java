281e0e28b09b17ae0ccd36b008267218b88b6782
trent <trent@DESKTOP-NKK8L7L>
2019-10-28 23:37:18 -0500

    Merge Trenton/master
    Conflicts:
    	src/main/java/salesTax.java

---
+++ src/main/java/chapter7Problem11/testDataResults.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter7Problem11;
+
+/**
+ *
+ * @author trent
+ */
+public class testDataResults {
+    private int[] testScores = new int[10];
+    
+    public static int testScoresDatabse(){
+    
+    
+    return 0;
+}
+    
+    
+    
+    
+}
